a1c313f8352c819df568142861d8e1acd40010ef
K.K.Rajpurohit <kkrajpurohit007@gmail.com>
2023-07-19 18:29:03 +0530

    update title

--- src/component/header/index.tsx
+++ src/component/header/index.tsx
@@ -5,7 +5,7 @@ export default function PageHeader() {
   return (
     <nav className="navbar is-info">
         <div className="navbar-brand">
-        <a className="navbar-item" id='app-title' href="#">{process.env.APPLICATION_NAME}Auction System</a>
+        <a className="navbar-item" id='app-title' href="#">Auction Bidding System</a>
         </div>
     </nav>
   )
